30413ba54c596598518e468afb48d9129bfd2cce
Geoff <geoff@gatwards.org>
2021-09-29 12:34:04 +1000

    HAProxy and OCP config

--- project_ocp/ipa_setup/ipa_dns_setup.sh
+++ project_ocp/ipa_setup/ipa_dns_setup.sh
@@ -4,6 +4,7 @@
 ipa dnszone-add ocp.home.gatwards.org. --allow-sync-ptr=true --dynamic-update=true
 ipa dnszone-add 5.22.172.in-addr.arpa. --name-from-ip=172.22.5.0 --allow-sync-ptr=true --dynamic-update=true
 
+ipa dnsrecord-add ocp.home.gatwards.org proxy --a-ip-address=172.22.5.99 --a-create-reverse
 ipa dnsrecord-add ocp.home.gatwards.org services --a-ip-address=172.22.5.100 --a-create-reverse
 ipa dnsrecord-add ocp.home.gatwards.org master1.lab --a-ip-address=172.22.5.101 --a-create-reverse
 ipa dnsrecord-add ocp.home.gatwards.org master2.lab --a-ip-address=172.22.5.102 --a-create-reverse
@@ -12,6 +13,6 @@ ipa dnsrecord-add ocp.home.gatwards.org bootstrap.lab --a-ip-address=172.22.5.11
 
 # OKD cluster internal IPs
 
-ipa dnsrecord-add ocp.home.gatwards.org api.lab --a-ip-address=172.22.5.100
-ipa dnsrecord-add ocp.home.gatwards.org api-int.lab --a-ip-address=172.22.5.100
-ipa dnsrecord-add ocp.home.gatwards.org *.apps.lab --a-ip-address=172.22.5.100
+ipa dnsrecord-add ocp.home.gatwards.org api.lab --a-ip-address=172.22.5.99
+ipa dnsrecord-add ocp.home.gatwards.org api-int.lab --a-ip-address=172.22.5.99
+ipa dnsrecord-add ocp.home.gatwards.org *.apps.lab --a-ip-address=172.22.5.99
